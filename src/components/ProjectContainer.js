b3a476cb30edd1a6a5b6351723eb465ed91527b1
teekaygithub <tomoyuki.kato@yahoo.com>
2022-04-15 23:32:29 -0700

    Converted ProjectContainer to a functional component with hooks

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -7,81 +7,73 @@ import { getProjects } from '../actions/projectActions';
 import Spinner from './Spinner';
 import '../dashboard.css';
 
-class ProjectContainer extends Component {
-    constructor(props) {
-        super(props);
-    }
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
-    componentDidMount() {
-        this.props.getProjects();
-    }
+function ProjectContainer() {
+    const myProjects = useSelector((state) => state.myprojects);
+    const dispatch = useDispatch();
 
-    render () {
-        if (!this.props.myprojects.loading) {
-            if (this.props.myprojects.projectlist.length > 0) {
-                const projectElem = this.props.myprojects.projectlist.map((el, index) => {
-                    return (
-                        <div key={index} className="project-card">
-                            <ProjectCard 
-                                title={el.title} 
-                                description={el.description} 
-                                PID={el.projectIdentifier}
-                                id={el.id}
-                                className="my-auto" />
-                        </div>
-                    );
-                });
-                return (
-                    <div className="container">
-                        <div id="project-banner">
-                            <h1 
-                                className="my-3"
-                                style={{textAlign:'center'}}>
-                                Your Active Projects
-                            </h1>
-                            <div className="d-flex justify-content-end w-75 my-4">
-                                <AddProject history={this.props.history} />
-                            </div>
-                        </div>
-                        <div
-                            id="project-container" >
-                            {projectElem}
-                        </div>
-                    </div>
-                );
-            } else {
-                return (
-                    <div className="container">
-                        <h1>Let's get started with a project!</h1>
-                        <AddProject />
-                    </div>
-                );
-            }
-        } else {
-            return(
-                <div className="dashboard-loading">
-                    <Spinner />
+    useEffect(() => {
+        // Dispatch action for getting all projects registered to a user
+        getProjects(dispatch);
+    }, []);
+
+    // List of all the project card elements
+    const projectElem = myProjects.projectlist.map((el, index) => {
+        return (
+            <div key={index} className="project-card">
+                <ProjectCard
+                    title={el.title}
+                    description={el.description}
+                    PID={el.projectIdentifier}
+                    id={el.id}
+                    className="my-auto" />
+            </div>
+        );
+    });
+
+    // Show this if user has at least one project
+    const projectView = (
+        <div className="container">
+            <div id="project-banner">
+                <h1
+                    className="my-3"
+                    style={{ textAlign: 'center' }}>
+                    Your Active Projects
+                </h1>
+                <div className="d-flex justify-content-end w-75 my-4">
+                    <AddProject />
                 </div>
-            );
-        }
-    }
+            </div>
+            <div
+                id="project-container" >
+                {projectElem}
+            </div>
+        </div>
+    );
+
+    const projectEmpty = (
+        <div className="container">
+            <h1>Let's get started with a project!</h1>
+            <AddProject />
+        </div>
+    );
+
+    const loadedView = myProjects.projectlist.length > 0 ? projectView : projectEmpty;
+    const loadingView = (
+        <div className="dashboard-loading">
+            <Spinner />
+        </div>
+    );
+
+    return (
+        <>
+            {
+                myProjects.loading ? loadingView : loadedView
+            }
+        </>
+    );
 }
 
-ProjectContainer.propTypes = {
-    getProjects: PropTypes.func.isRequired,
-    myprojects: PropTypes.object.isRequired,
-    userauth: PropTypes.object.isRequired
-}
-
-const mapStateToProps = (state) => ({
-    myprojects: state.myprojects,
-    userauth: state.userauth
-});
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getProjects: () => {getProjects(dispatch);}
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectContainer);
+export default ProjectContainer;
