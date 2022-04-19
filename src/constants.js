2d9aaed8c63a6c90d0e5b8850941c1f7a7921541
teekaygithub <tomoyuki.kato@yahoo.com>
2022-04-18 21:46:22 -0700

    Updated backend API to HTTPS

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ var API = "";
 // if (process.env.NODE_ENV == 'production') {
 //     API = "http://mykanbanbe-env.eba-53cbc8m6.us-west-1.elasticbeanstalk.com/";
 // } else {
-    API = "http://localhost:8080/"
+    API = "https://localhost:8443/"
 // }
 
 export var API;
