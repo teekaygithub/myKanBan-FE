d8a56aa1ffd441b334509e163176b492e10b00b7
teekaygithub <tomoyuki.kato@yahoo.com>
2022-04-24 18:50:53 -0700

    Social links in footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faGithub, faTiktok, faYoutube } from "@fortawesome/free-brands-svg-icons"
 
 function Footer() {
     return (
         <footer>
             <div id="footer-socials">
-                <p>TBD: Social Media Links</p>
+                <p>Social Links</p>
+                <div id="footer-social-link-wrapper">
+                    <a href="https://www.facebook.com" target="_blank" className="footer-social-link"><FontAwesomeIcon icon={faFacebook} /></a>
+                    <a href="https://www.youtube.com" target="_blank" className="footer-social-link"><FontAwesomeIcon icon={faYoutube} /></a>
+                    <a href="https://www.tiktok.com" target="_blank" className="footer-social-link"><FontAwesomeIcon icon={faTiktok} /></a>
+                    <a href="https://www.github.com" target="_blank" className="footer-social-link"><FontAwesomeIcon icon={faGithub} /></a>
+                </div>
             </div>
             <div id="footer-links">
                 <ul>
